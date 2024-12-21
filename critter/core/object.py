0f285fe9471b5afa93a891b25710e1aaae64e1ca
DragonMoffon <happydog.aja@gmail.com>
2024-12-21 23:55:07 +1300

    start on movement and isometric shaders

--- critter/core/object.py
+++ critter/core/object.py
@@ -1,30 +1,101 @@
 from __future__ import annotations
+from typing import Deque, Any
+from array import array
+from collections import deque
+from arcade import ArcadeContext
 
-class Attribute:
+class Attribute[T]:
     
     def __init__(self, target: str):
         self.target = target
 
-    def __get__(self, obj: Object, objtype=None):
-        return obj.source[self.target][obj.idx]
+    def __get__(self, obj: Object, objtype=None) -> T:
+        return obj.source.get(obj.idx, self.target)
 
-    def __set__(self, obj: Object, value):
-        obj.source[self.target][obj.idx] = value
+    def __set__(self, obj: Object, value: T):
+        obj.source.set(obj.idx, self.target, value)
 
 class Object:
 
     def __init__(self, idx: int, source: ObjectList):
-        self.idx: int = 0
-        self.source: ObjectList = None
+        self.idx: int = idx
+        self.source: ObjectList = source
+
+
+_CAPACITY_DEFAULT = 128
+class SyncedArray:
+
+    def __init__(self, dtype, step, size):
+        self._array = None
+        self._buffer = None
+
+        self._size = size
+        self._dtype = dtype
+        self._step = step
+
+        self._initialised = False
+
+    def initialise(self, ctx: ArcadeContext):
+        self._array = array(self._dtype, [0] * self._size * self._step)
+        self._buffer = ctx.buffer(reserve=4 * self._size * self._step) # TODO take into account dtype size
+
+    def __getitem__(self, idx: int) -> Any:
+        return tuple(self._array[idx*self._step + i] for i in range(self._step))
+
+    def __setitem__(self, idx: int, value: Any):
+        self._array[]
 
 
 class ObjectList:
 
-    def __init__(self, capacity: int = 1024, lazy: bool = True):
-        self._slots
+    def __init__(self, targets: tuple[tuple[str, str, int], ...], capacity: int = _CAPACITY_DEFAULT, lazy: bool = True):
+        self._lazy = lazy
+        self._cpu_stale = False
+        self._gpu_stale = False
+
+        self._capacity = self._slot_capacity = abs(capacity) or _CAPACITY_DEFAULT
+        self._freed_slots: Deque[int] = deque()
+        self._used_slots: int = 0
+
+        self._targets: dict[str, SyncedArray] = {}
     
-    def __get_item__(self, key):
-        pass
+    def _next_slot(self) -> int:
+        """
+        Get the next available slot in the buffers
+
+        Returns:
+            slot index
+        """
+        if self._freed_slots:
+            return self._freed_slots.popleft()
+        
+        slot = self._used_slots
+        self._used_slots += 1
+        self._grow_buffers()
+        return slot
+    
+    def _grow_buffers(self):
+        if self._used_slots <= self._capacity:
+            return
+        
+        extend_by = self._capacity
+        self._capacity *= 2
+
+
 
     def new(self) -> int:
         pass
+
+    def rem(self, idx: int) -> None:
+        pass
+
+    def get(self, idx: int, target: str) -> Any:
+        pass
+
+    def set(self, idx: int, target: str, value: Any):
+        pass
+
+
+
+class Sprite(Object):
+    position = Attribute[tuple[int, int, int]]("position")
---
+++ resources/shaders/isometric_sprite_vs.glsl
@@ -0,0 +1,28 @@
+#version 330
+
+uniform vec3 scale;
+
+in vec3 in_pos;
+in float in_angle;
+in vec2 in_size;
+in float in_texture;
+in vec4 in_color;
+
+out float v_angle;
+out vec4 v_color;
+out vec2 v_size;
+out float v_texture;
+
+void main() {
+    vec3 pos = vec3(
+        (in_pos.x - in_pos.y) * scale.x,
+        (in_pos.y + in_pos.x) * scale.y + in_pos.z * scale.z + in_size.y / 2.0,
+        -(in_pos.y + in_pos.x) * scale.y
+    );
+
+    gl_Position = vec4(pos, 1.0);
+    v_angle = in_angle;
+    v_color = in_color;
+    v_size = in_size;
+    v_texture = in_texture;
+}
